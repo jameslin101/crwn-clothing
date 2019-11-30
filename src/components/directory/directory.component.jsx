8bc2433ecdc935ec602bbd5dfa67114d3fc7f314
James Lin <jameslin101@gmail.com>
2019-11-29 22:29:48 -0500

    update menu-items to navigate

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component';
 
@@ -48,8 +47,8 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          this.state.sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
